24367213533e9d1d65752e579c289e4774697a6d
Carlos A. Castro <chali.castro@gmail.com>
2025-08-08 19:56:24 +0200

    Configuracion inicial

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Rooms from './components/Rooms.vue'
+</script>
+
+<template>
+  <!-- <div>{{ %VITE_GEMINI_API_KEY% }}</div> -->
+  <v-layout>
+    <Rooms />
+  </v-layout>
+</template>
---
+++ src/components/Rooms.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { collection, getDocs } from 'firebase/firestore';
+import { firestore } from '../main'; // Assuming you have a firebase.js file exporting 'db'
+import { CRoom } from '../model/room';
+
+const rooms = ref<Array<CRoom>>([]);
+
+onMounted(async () => {
+  const querySnapshot = await getDocs(collection(firestore, 'rooms'));
+  rooms.value = querySnapshot.docs.map(doc => new CRoom(doc.id, doc.data()));
+});
+</script>
+
+<template>
+  <div>
+    <h2>Rooms</h2>
+    <ul>
+      <li v-for="room in rooms" :key="room.id">{{ room.name }}</li>
+    </ul>
+  </div>
+</template>
+
+
+<style scoped>
+/* Add your styles here */
+</style>
