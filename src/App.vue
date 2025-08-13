12941322731975ef2cc377893654c2b5717b46c7
Carlos A. Castro <chali.castro@gmail.com>
2025-08-13 17:41:47 +0200

    Octavo avance: fix adsense IV

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
-import { nextTick, onMounted } from 'vue';
-import { adsenseClient } from './firebase';
-
-onMounted(async () =>
-{
-  await nextTick();
-  const window: any = document.defaultView;
-  console.log(window.adsbygoogle, adsenseClient);
-  console.log(window);
-  window.adsbygoogle = (window.adsbygoogle || []).push({});
-  console.log(window.adsbygoogle);
-});
+import { onMounted, ref } from 'vue';
 </script>
-  
+
 <template>
   <v-app>
     <v-layout>
       <router-view />
     </v-layout>
   </v-app>
-   <ins class="adsbygoogle"
-    :data-ad-client="adsenseClient"
-    data-ad-slot="1234567890"
-    data-ad-format="auto"></ins>
 </template>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { nextTick, onMounted, ref } from 'vue';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useFirebaseAuth } from 'vuefire';
+import { adsenseClient } from '../firebase';
 
 const auth = useFirebaseAuth();
 const email = ref('');
@@ -13,18 +14,47 @@ const login = async () =>
         await signInWithEmailAndPassword(auth, email.value, password.value);
     }
 };
+
+const adsbygoogle = ref();
+
+onMounted(async () =>
+{
+    await nextTick();
+    const window: any = document.defaultView;
+    console.log(window.adsbygoogle, adsenseClient);
+    console.log(window);
+    if (adsbygoogle.value && window.adsbygoogle) {
+        (adsbygoogle.value = window.adsbygoogle || []).push({});
+    }
+});
+
 </script>
 <template>
     <v-main class="d-flex align-center justify-center">
         <v-container>
-            <v-form>
-                <v-text-field label="Email"
-                    v-model="email" />
-                <v-text-field label="Password"
-                    type="password"
-                    v-model="password" />
-                <v-btn @click="login">Login</v-btn>
-            </v-form>
+            <v-row>
+                <v-col style="height: 50vh;">
+                    <v-form>
+                        <v-text-field label="Email"
+                            v-model="email" />
+                        <v-text-field label="Password"
+                            type="password"
+                            v-model="password" />
+                        <v-btn @click="login">Login</v-btn>
+                    </v-form>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col style="height: 30vh;">
+                    <ins ref="adsbygoogle"
+                        class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-2014966493239600"
+                        data-ad-slot="1234567890"
+                        data-full-width-responsive="true"
+                        data-ad-format="rectangle"></ins>
+                </v-col>
+            </v-row>
         </v-container>
     </v-main>
 </template>
